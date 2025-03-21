6a8f0fb3d903ea834d144b0b9e9a969bb787cc58
daniel <danielreubenkelly@gmail.com>
2025-03-21 23:51:17 +0000

    updated windows ouput text for -l to properly format

--- interfacePrint/printInterfaces_win.go
+++ interfacePrint/printInterfaces_win.go
@@ -11,18 +11,18 @@ import (
 
 func PrintInterfaces(nics []pcap.Interface) (bool, string) {
 	colourtext.PrintInfo("devices: ")
-	header := fmt.Sprintf("%10s %29s %13s", "name", "description", "IP")
+	header := fmt.Sprintf("%10s %54s %31s", "name", "IP", "description")
 	colourtext.PrintColour(colourtext.Cyan, header)
 	for i, dev := range nics {
 		switch len(dev.Addresses) {
 		case 0:
-			line := fmt.Sprintf("%-5s %-20s | %-20s |", "["+fmt.Sprint(i)+"]", dev.Name, dev.Description)
+			line := fmt.Sprintf("%-5s %-54s | %-20s | %-20s", "["+fmt.Sprint(i)+"]", dev.Name, "", dev.Description)
 			colourtext.PrintColour(colourtext.Cyan, line)
 		case 1:
-			line := fmt.Sprintf("%-5s %-20s | %-20s |", "["+fmt.Sprint(i)+"]", dev.Name, dev.Description)
+			line := fmt.Sprintf("%-5s %-54s | %-20s | %-20s", "["+fmt.Sprint(i)+"]", dev.Name, "", dev.Description)
 			colourtext.PrintColour(colourtext.Cyan, line)
 		case 2:
-			line := fmt.Sprintf("%-5s %-20s | %-20s | %-20s", "["+fmt.Sprint(i)+"]", dev.Addresses[1].IP.String(), dev.Name, dev.Description)
+			line := fmt.Sprintf("%-5s %-54s | %-20s | %-20s", "["+fmt.Sprint(i)+"]", dev.Name, dev.Addresses[1].IP.String(), dev.Description)
 			//line := "[" + fmt.Sprint(i) + "] " + dev.Name + " | " + dev.Description + " | " + dev.Addresses[1].IP.String()
 			colourtext.PrintColour(colourtext.Cyan, line)
 		}
